81a4cb765b8e99d7f90c3e4720d7a2c37d7fce94
sleezyx <kareem.elew@gmail.com>
2022-10-10 19:23:08 +0200

    Updated Home Screen & Footer

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -6,6 +6,9 @@ export default function Intro() {
       <div className="wrapper">
         <p className="landing-text">HELLO WORLD! I'M</p>
         <h1 className="landing-name">KAREEM ELEWA</h1>
+        <a href="#about">
+          <div className="scroll-down"></div>
+        </a>
       </div>
     </div>
   )
